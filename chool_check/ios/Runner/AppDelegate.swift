2910b984481b230bacc7c26cd4602a9c9c1f5ebc
dhko <kodh1994@naver.com>
2023-07-09 16:29:16 +0900

    다섯번쨰 오호

--- chool_check/ios/Runner/AppDelegate.swift
+++ chool_check/ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyC-VqIrirqtan_9tWoqdyJkOzDxotaN_1A")
+    GMSServices.provideAPIKey("api-key")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- chool_check/lib/screen/home_screen.dart
+++ chool_check/lib/screen/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-// AIzaSyC-VqIrirqtan_9tWoqdyJkOzDxotaN_1A
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
